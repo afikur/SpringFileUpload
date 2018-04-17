50a24927e4e4a2cec8111a867429ee06d42018bb
Afikur Rahman <afikurkhan.dsi@gmail.com>
2018-04-17 20:58:25 +0600

    init commit

---
+++ src/java/com/afikur/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.afikur.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.afikur"})
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver resourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(5545394);
+        return multipartResolver;
+    }
+}
---
+++ src/java/com/afikur/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.afikur.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("/home")
+    public String welcome() {
+        return "welcome";
+    }
+}
